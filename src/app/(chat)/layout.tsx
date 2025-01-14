a521374b6a306384950453e11bb0d5bb7dd36974
Arthur Geel <geelaj01@gmail.com>
2025-01-14 13:32:24 +0100

    Use lightmode by default

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`antialiased tracking-tight ${inter.className}`}>
-        <ThemeProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider attribute="class" defaultTheme="light">
           {children}
           <Toaster />
         </ThemeProvider>
